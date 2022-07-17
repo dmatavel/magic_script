8d7ce949d803dd33458319b0cc2103e0909001b3
dmatavel <matavelidanilo@gmail.com>
2022-07-17 13:47:29 -0300

    uploading magic_script

---
+++ magic_script.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+SU=sudo
+SAPT="sudo apt"
+INST=install
+RM=remove
+
+echo Updating system ...
+
+    $SAPT update && sudo apt upgrade
+
+echo Installing general tools ...
+echo Version control system ...
+    $SAPT $INST git
+
+echo Text editors ...
+    $SAPT $INST vim
+    $SAPT $INST neovim
+
+echo Codecs and fonts
+    $SAPT $INST ubuntu-restricted-extras -y
+    $SAPT $INST ffmpeg
+
+echo Installing C compiler and other C tools ...
+    $SAPT $INST build-essential
+    $SAPT $INST libbsd-dev
+    $SAPT $INST ccache
+    $SAPT $INST clang
+    $SAPT $INST valgrind
+
+echo Installing Python3 and pip modules ...
+    $SAPT $INST python3
+    $SAPT $INST python3-pip
+
+echo Installing 42 norminette ...
+    $SU python3 -m pip install --upgrade pip setuptools
+    $SU python3 -m pip install norminette
+
+echo Setting a SSH key ...
+    ssh-keygen
+
+echo Remove unnacessary files and packages ...
+    $SAPT clean
+    $SAPT auto-clean
+    $SAPT autoremove
